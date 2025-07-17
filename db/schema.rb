56e30afd6f2083cc6db8ef098cc5e89d10e3cd4d
diego vidal <diego27796gagan@gmail.com>
2025-07-16 22:46:29 -0600

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_17_044025) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_17_044448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_17_044025) do
     t.date "start_date"
     t.date "end_date"
     t.decimal "total_amount", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
