1a82c7aadb6016ce638bccad685b6d4833883ccd
Diego Vidal <diego27796gagan@gmail.com>
2025-12-21 01:21:39 -0600

    generate predetermiend default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# Default Categories
+default_categories = [
+  "Food",
+  "Transport",
+  "Taxes",
+  "Commissions and Interest",
+  "Home"
+]
+
+default_categories.each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
+
+puts "Seeded #{default_categories.count} default categories"
