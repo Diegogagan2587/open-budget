c128df20787c6a68eece7ade1643673a4c87fcb4
Diego Vidal Lopez <diego27796gagan@gmail.com>
2025-12-29 12:45:21 -0600

    feat(User): validates presence of name, email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :name, presence: true
+  validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || !password.nil? }
 end
