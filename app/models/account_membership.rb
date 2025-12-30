90483d0a18468deb894a7e996f5714609c536488
Diego Vidal Lopez <diego27796gagan@gmail.com>
2025-12-29 19:30:29 -0600

    feat(AcccountMemeership): create model, belongs to user and acccount, validates role, user_id, soped to owners and members, inlucd method owner?

---
+++ app/models/account_membership.rb
@@ -0,0 +1,15 @@
+class AccountMembership < ApplicationRecord
+  belongs_to :user
+  belongs_to :account
+
+  validates :role, presence: true, inclusion: { in: %w[owner member] }
+  validates :user_id, uniqueness: { scope: :account_id }
+
+  scope :owners, -> { where(role: 'owner') }
+  scope :members, -> { where(role: 'member') }
+
+  def owner?
+    role == 'owner'
+  end
+end
+
