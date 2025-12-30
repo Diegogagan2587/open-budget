55f73a744a4272763414b154deb92482f44da3d5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2025-12-29 20:30:09 -0600

    feat(application_controller.rb): include method to set current account scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   include Authentication
+  include AccountScoping
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def current_account_scope
+    Current.account
+  end
 end
+
