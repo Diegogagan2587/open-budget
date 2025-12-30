e2a60d733e5f4bee9837a65edaa36da0cdb3ec0e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2025-12-29 19:25:20 -0600

    feat(migrate/..._add_name_to_users.rb): generate default username for existing users

--- db/migrate/20251229183551_add_name_to_users.rb
+++ db/migrate/20251229183551_add_name_to_users.rb
@@ -1,5 +1,12 @@
 class AddNameToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :name, :string, null: false
+  def up
+    add_column :users, :name, :string, null: true
+    # Set default name for existing users
+    execute "UPDATE users SET name = split_part(email_address, '@', 1) WHERE name IS NULL"
+    change_column_null :users, :name, false
+  end
+
+  def down
+    remove_column :users, :name
   end
 end
