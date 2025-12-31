fcd7f320434b13d44f5ebfa0d2f95064f8881305
Diego Vidal Lopez <diego27796gagan@gmail.com>
2025-12-30 18:44:48 -0600

    feat(test_helper.rb): define helper method for simulating auth on tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,25 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+# Helper for integration tests to set up authentication
+module ActionDispatch
+  class IntegrationTest
+    def sign_in_as(user, account = nil)
+      account ||= user.accounts.first
+      session = user.sessions.create!(
+        user_agent: "Test Agent",
+        ip_address: "127.0.0.1"
+      )
+      # Set Current values that will be used by authentication
+      Current.session = session
+      Current.account = account
+      # Also try to set cookie if possible
+      if respond_to?(:cookies) && cookies.respond_to?(:[]=)
+        # For integration tests, we'll rely on Current.session being set
+        # The authentication concern checks Current.session first
+      end
+      session
+    end
+  end
+end
