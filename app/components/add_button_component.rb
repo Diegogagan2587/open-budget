cee6db2753f2bc238d0dedb846450029b121d52d
Diego Vidal <diego27796gagan@gmail.com>
2026-01-02 14:12:46 -0600

    feat(AddButtonComponent): define css classes for styling

--- app/components/add_button_component.rb
+++ app/components/add_button_component.rb
@@ -5,4 +5,8 @@ class AddButtonComponent < ViewComponent::Base
     @label = label
     @path  = path
   end
+
+  def classes
+   "px-4 py-2 bg-gradient-to-r from-green-500 to-green-600 text-white rounded shadow-md hover:shadow-lg hover:from-green-600 hover:to-green700 transition-all duration-200 flex items-center gap-2"
+  end
 end
