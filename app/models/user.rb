225edf29ca1196b1cb986b9de38c0acb8f583b2d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-01-29 14:44:00 -0600

    feat(User): validtes inclusion of :locale es and en, allow_nil is true

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email_address, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, length: { minimum: 8 }, if: -> { new_record? || !password.nil? }
+  validates :locale, inclusion: { in: %w[en es] }, allow_nil: true
 
   def owned_accounts
     accounts.joins(:account_memberships).where(account_memberships: { role: "owner" })
