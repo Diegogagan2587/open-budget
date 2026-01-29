755a3658cbbe8f3e5043ff8a51944c6beb2c8419
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-01-29 14:47:06 -0600

    feat(ApplicationController): set locale before action, define set_locale method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,20 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :set_locale
+
   private
+
+  def set_locale
+    locale = if Current.user&.locale.present? && I18n.available_locales.map(&:to_s).include?(Current.user.locale)
+      Current.user.locale
+    elsif session[:locale].present? && I18n.available_locales.map(&:to_s).include?(session[:locale].to_s)
+      session[:locale].to_s
+    else
+      I18n.default_locale
+    end
+    I18n.locale = locale.to_sym
+  end
 
   def current_account_scope
     Current.account
