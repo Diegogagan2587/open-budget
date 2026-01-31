1021f6cbaf2f14860f37cf9cf45516c354e88cb9
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-01-31 13:07:59 -0600

    rubocop -A

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module DiegoApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.available_locales = [:en, :es]
+    config.i18n.available_locales = [ :en, :es ]
   end
 end
