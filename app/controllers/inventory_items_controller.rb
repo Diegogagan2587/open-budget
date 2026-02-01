6f426be6b46a42263d45aad72a3876914f4d535d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-02-01 14:23:15 -0600

    feat(inventry_items): add filter by category

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -5,11 +5,28 @@ class InventoryItemsController < ApplicationController
 
   def index
     @stock_filter = params[:stock_state] || "all"
+    @category_filter = params[:category_id].presence
+    @category_filter = nil if @category_filter == "all"
+
     @inventory_items = InventoryItem.for_account(Current.account).includes(:category)
 
     if @stock_filter != "all"
       @inventory_items = @inventory_items.where(stock_state: @stock_filter)
     end
+
+    if @category_filter.present?
+      @inventory_items = if @category_filter == "uncategorized"
+        @inventory_items.where(category_id: nil)
+      else
+        @inventory_items.where(category_id: @category_filter)
+      end
+    end
+
+    @categories_for_filter = Category.for_account(Current.account)
+      .joins(:inventory_items)
+      .where(inventory_items: { account_id: Current.account.id })
+      .distinct
+      .order(:name)
 
     @grouped_inventory_items = build_grouped_inventory_items(@inventory_items)
   end
@@ -59,34 +76,36 @@ class InventoryItemsController < ApplicationController
     @inventory_item.destroy!
 
     respond_to do |format|
-      format.html { redirect_to inventory_items_path, status: :see_other, notice: t("inventory_items.flash.destroyed") }
+      format.html { redirect_to inventory_items_path(**index_path_options_for_redirect), status: :see_other, notice: t("inventory_items.flash.destroyed") }
       format.json { head :no_content }
     end
   end
 
   def add_to_shopping_list
     @inventory_item.add_to_shopping_list!
-    redirect_to inventory_items_path(stock_state: params[:stock_state].presence || "all"), notice: t("inventory_items.flash.added_to_shopping_list")
+    redirect_to inventory_items_path(**index_path_options_for_redirect), notice: t("inventory_items.flash.added_to_shopping_list")
   end
 
   def update_stock_state
     @stock_filter = params[:stock_state_filter].presence || "all"
+    @category_filter = params[:category_id].presence
+    @category_filter = nil if @category_filter == "all"
     new_state = params[:stock_state].presence_in(%w[in_stock low empty])
     if new_state
       @inventory_item.update!(stock_state: new_state)
       respond_to do |format|
-        format.html { redirect_to inventory_items_path(stock_state: @stock_filter), notice: t("inventory_items.flash.updated") }
+        format.html { redirect_to inventory_items_path(**index_path_options), notice: t("inventory_items.flash.updated") }
         format.turbo_stream do
           if @stock_filter != "all" && @inventory_item.stock_state != @stock_filter
             render turbo_stream: turbo_stream.remove(dom_id(@inventory_item)), status: :ok
           else
-            render turbo_stream: turbo_stream.replace(dom_id(@inventory_item), partial: "inventory_items/card", locals: { inventory_item: @inventory_item, stock_filter: @stock_filter }), status: :ok
+            render turbo_stream: turbo_stream.replace(dom_id(@inventory_item), partial: "inventory_items/card", locals: { inventory_item: @inventory_item, stock_filter: @stock_filter, category_filter: @category_filter }), status: :ok
           end
         end
       end
     else
       respond_to do |format|
-        format.html { redirect_to inventory_items_path(stock_state: @stock_filter), alert: t("inventory_items.flash.invalid_stock_state") }
+        format.html { redirect_to inventory_items_path(**index_path_options), alert: t("inventory_items.flash.invalid_stock_state") }
         format.turbo_stream { head :unprocessable_entity }
       end
     end
@@ -100,6 +119,19 @@ class InventoryItemsController < ApplicationController
 
   def inventory_item_params
     params.expect(inventory_item: [ :name, :stock_state, :consumable, :category_id, :notes ])
+  end
+
+  def index_path_options
+    opts = { stock_state: @stock_filter }
+    opts[:category_id] = @category_filter if @category_filter.present?
+    opts
+  end
+
+  def index_path_options_for_redirect
+    @stock_filter = params[:stock_state].presence || "all"
+    @category_filter = params[:category_id].presence
+    @category_filter = nil if @category_filter == "all"
+    index_path_options
   end
 
   def build_grouped_inventory_items(items)
