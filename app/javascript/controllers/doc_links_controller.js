08be83a16b8868854474550bb2f26e91fadcfffd
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-02-22 17:25:43 -0600

    fea(doc_links_controller.js): filter link using user input to make esasier addition of links on docs

--- app/javascript/controllers/doc_links_controller.js
+++ app/javascript/controllers/doc_links_controller.js
@@ -1,9 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "search", "linkItem"]
 
   toggleForm() {
     this.formTarget.classList.toggle("hidden")
+    if (!this.formTarget.classList.contains("hidden")) {
+      this.searchTarget.value = ""
+      this.filterLinks()
+      this.searchTarget.focus()
+    }
+  }
+
+  filterLinks() {
+    const searchTerm = this.searchTarget.value.toLowerCase()
+    
+    this.linkItemTargets.forEach(item => {
+      const title = item.dataset.linkTitle
+      const url = item.dataset.linkUrl
+      const matches = title.includes(searchTerm) || url.includes(searchTerm)
+      
+      item.closest('form').classList.toggle('hidden', !matches)
+    })
   }
 }
