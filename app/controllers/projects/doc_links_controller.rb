7e9a9c07557e71a97e6358b043e4bccacf054c6f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-02-22 17:10:54 -0600

    feat(Projects::DocLinksController): create and destroy relation ships between docs and links

---
+++ app/controllers/projects/doc_links_controller.rb
@@ -0,0 +1,33 @@
+module Projects
+  class DocLinksController < ApplicationController
+    before_action :set_doc
+    before_action :set_link, only: [:destroy]
+
+    def create
+      @link = Projects::Link.for_account(Current.account).find(params[:link_id])
+      
+      if @doc.links << @link
+        redirect_to projects_doc_path(@doc), notice: t('projects.flash.link_added')
+      else
+        redirect_to projects_doc_path(@doc), alert: t('projects.flash.link_add_failed')
+      end
+    rescue ActiveRecord::RecordInvalid
+      redirect_to projects_doc_path(@doc), alert: t('projects.flash.link_already_added')
+    end
+
+    def destroy
+      @doc.links.delete(@link)
+      redirect_to projects_doc_path(@doc), notice: t('projects.flash.link_removed')
+    end
+
+    private
+
+    def set_doc
+      @doc = Projects::Doc.for_account(Current.account).find(params[:doc_id])
+    end
+
+    def set_link
+      @link = @doc.links.find(params[:id])
+    end
+  end
+end
