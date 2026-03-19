9f3c830d232c4162e21952cd741c2127539d3038
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-03-18 23:19:34 -0600

    jobs(helloJob): print test greeint on consle

---
+++ app/jobs/hello_job.rb
@@ -0,0 +1,10 @@
+class HelloJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    Rails.logger.info "[HeloJob] Hi from hello job"
+    # we console.log if we are on development
+    if Rails.env.development?
+      puts "[HelloJob]: Hi from Hello job"
+  end
+end
