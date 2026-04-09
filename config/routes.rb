a47ceee51b75c13227e43f4b3dbf306e4585155f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-04-08 21:19:14 -0600

    config(Routes): add nested categories resource under settings/finance namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resource :session
   resource :registration, only: [ :new, :create ]
   resource :settings, only: [ :edit, :update ]
+  namespace :settings do
+    namespace :finance do
+      resources :categories, controller: "/categories"
+    end
+  end
   resources :passwords, param: :token
   get "dashboard/index"
   # resources :budget_line_items
