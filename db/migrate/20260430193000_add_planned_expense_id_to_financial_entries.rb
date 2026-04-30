f9a85d4a2343b35e484ebe1e39a76633e3de7ab5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-04-30 09:29:43 -0600

    feat(db): link financial entries to planned expenses

---
+++ db/migrate/20260430193000_add_planned_expense_id_to_financial_entries.rb
@@ -0,0 +1,5 @@
+class AddPlannedExpenseIdToFinancialEntries < ActiveRecord::Migration[8.0]
+  def change
+    add_reference :financial_entries, :planned_expense, null: true, foreign_key: true
+  end
+end
