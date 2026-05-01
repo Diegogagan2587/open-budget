e05e36db67ff62c12d9f99982f7c28b1077f0878
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-05-01 16:51:29 -0600

    test(SidebarHeaderComponent): add component coverage

---
+++ test/components/sidebar_header_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SidebarHeaderComponentTest < ViewComponent::TestCase
+  def test_component_renders_brand_link
+    rendered = render_inline(SidebarHeaderComponent.new)
+
+    assert rendered.css("a[href='#{root_path}']").any?
+    assert_includes rendered.text, "Open Budget"
+  end
+end
