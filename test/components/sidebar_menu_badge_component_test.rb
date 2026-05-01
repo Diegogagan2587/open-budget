b6edd35408c64b5f637c3fcf281b79310578b896
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-05-01 16:51:29 -0600

    test(SidebarMenuBadgeComponent): add component coverage

---
+++ test/components/sidebar_menu_badge_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SidebarMenuBadgeComponentTest < ViewComponent::TestCase
+  def test_component_renders_badge_span
+    rendered = render_inline(SidebarMenuBadgeComponent.new) { "3" }
+
+    assert rendered.css("span").any?
+    assert_includes rendered.text, "3"
+  end
+end
